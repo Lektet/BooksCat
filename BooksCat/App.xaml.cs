21f3a4cff3bbd72e9953afdee8979c1389b40f45
Lektet <Lektet@users.noreply.github.com>
2015-08-04 16:11:01 +0300

    List view added. String resource files added.

--- BooksCat/App.xaml.cs
+++ BooksCat/App.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace BooksCat
 {
@@ -13,5 +16,20 @@ namespace BooksCat
     /// </summary>
     public partial class App : Application
     {
+        /*
+        Language switch (non-working)
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("ru-RU"); ;
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru-RU"); ;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+              typeof(FrameworkElement),
+              new FrameworkPropertyMetadata(
+                    XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            base.OnStartup(e);
+        }
+        */
     }
 }
